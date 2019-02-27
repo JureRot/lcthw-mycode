d84e0832ac34595193133a6f605f78707fa34027
NeumnoTele <jure.rot96@gmail.com>
2019-02-27 18:50:56 +0100

    ex24 testing (still no ec)

--- ex24testing.c
+++ ex24testing.c
@@ -4,35 +4,47 @@
 #include "dbg.h"
 
 #define MAX_DATA 1024
+#define NUM_LINES 5
 
 int main(int argc, char *argv[]) {
     FILE *in;
     FILE *out;
     char *filename = "IOin.txt";
     char line[MAX_DATA];
-    char **input;
+    char input[NUM_LINES][MAX_DATA];
+    int c = 0;
 
     in = fopen(filename, "r");
     out = fopen("IOout.txt", "w");
     check(in != NULL, "Couldn't open file %s", filename);
+    check(out != NULL, "Couldn't write to file IOout.txt");
 
-    int c = 0;
     while (fgets(line, MAX_DATA, in) != NULL) {
-        char *line_ptr = malloc(sizeof(line));
-        memcpy(line_ptr, line, strlen(line));
-        printf("%p, %s\n", *line_ptr, line_ptr);
-        //input[c++] = line_ptr;
-        fprintf(out, "The line was: %s", line);
-        free(line_ptr);
+        //input[c] = malloc(strlen(line));
+        strncpy(input[c], line, MAX_DATA);
+        input[c][strlen(line)-1] = '\0'; //replace \n with \0
+        c++;
+        fprintf(out, "The line of len %lu was: %s", strlen(line), line);
     }
 
     fclose(in);
     fclose(out);
 
+    printf("%lu %lu\n", sizeof(input), sizeof(input[0]));
+    for (int i=0; i<(sizeof(input)/sizeof(input[0])); i++) {
+        printf("%s\n", input[i]);
+    }
 
-    //we still need to free every line in input)
+    //if we have a fixed size of input array, we dont need to free it (there is no heap alloc (strcpy doesnt allocate))
+    //what about if we had a dynamic size of array
 
     return 0;
 error:
+    if (in) {
+        fclose(in);
+    }
+    if (out) {
+        fclose(out);
+    }
     return 1;
 }
