982adb985c6676c67e53068aceec9314e89523f3
NeumnoTele <jure.rot96@gmail.com>
2019-02-28 17:38:02 +0100

    ex24testing compromise

--- ex24testing.c
+++ ex24testing.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     char *filename = "IOin.txt";
     char line[MAX_DATA];
     char input[NUM_LINES][MAX_DATA];
-    //char *input[NUM_LINES];
+    char **input_dyn[NUM_LINES];
     int c = 0;
 
     in = fopen(filename, "r");
@@ -21,25 +21,39 @@ int main(int argc, char *argv[]) {
     check(out != NULL, "Couldn't write to file IOout.txt");
 
     while (fgets(line, MAX_DATA, in) != NULL) {
-        //input[c] = malloc(strlen(line));
+        line[strlen(line)-1] = '\0'; //replace \n at the end with \0
+        //static input
         strncpy(input[c], line, MAX_DATA);
-        input[c][strlen(line)-1] = '\0'; //replace \n with \0
+        //input[c][strlen(line)-1] = '\0'; //already replaced the \n at the end
+        //dynamic input
+        input_dyn[c] = malloc(strlen(line)*sizeof(char));
+        strncpy(input_dyn[c], line, strlen(line));
         c++;
         fprintf(out, "The line of len %lu was: %s", strlen(line), line);
     }
 
+
     fclose(in);
     fclose(out);
 
-    printf("%lu %lu\n", sizeof(input), sizeof(input[0]));
+    printf("input: %lu, input[0]: %lu\n", sizeof(input), sizeof(input[0]));
     for (int i=0; i<(sizeof(input)/sizeof(input[0])); i++) {
-        printf("%s\n", input[i]);
+        printf("input: %s\n", input[i]);
+    }
+
+    printf("\ninput_dyn: %lu, input_dyn[0]: %lu\n", sizeof(input_dyn), sizeof(input_dyn[0]));
+    for (int i=0; i<(sizeof(input_dyn)/sizeof(input_dyn[0])); i++) {
+        printf("input_dyn: %s\n", input_dyn[i]);
+        free(input_dyn[i]);
     }
 
     //if we have a fixed size of input array, we dont need to free it (there is no heap alloc (strcpy doesnt allocate))
-    //what about if we had a dynamic size of array (probably with pointers, but how, try that too)
     //and of course, do the standard ec
 
+    //what about if we had a dynamic size of array (probably with pointers, but how, try that too)
+    //i kinda made it work with input_dyn, and it works and all, BUT, it does throw some warning when compiling (wrong strncpy pointer and wrong type %s when printing)
+    //and still i dont know of a easy way of making array len NUM_LINES independen (assigning a dynamic size var after check doesnt bode well) (but for now its just easier to just declare a global NUM_LINES when doing something like that)
+    
     return 0;
 error:
     if (in) {
@@ -47,6 +61,9 @@ error:
     }
     if (out) {
         fclose(out);
+    }
+    for (int i=0; i<(sizeof(input_dyn)/sizeof(input_dyn[0])); i++) {
+        free(input_dyn[i]);
     }
     return 1;
 }
