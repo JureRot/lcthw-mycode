8a56d048559efb4591654541cc28c896b38faf50
NeumnoTele <jure.rot96@gmail.com>
2019-03-12 17:39:58 +0100

    ex33 is hard

--- liblcthw/src/lcthw/list_algos.c
+++ liblcthw/src/lcthw/list_algos.c
@@ -9,17 +9,17 @@
 //example: wxabyz -> wxbayz
 //TODO
 void swap_nodes(List *list, ListNode *a, ListNode *b) {
-    printf("%s %s\n", a->value, b->value);
     //if a is first element in list
     //if b is last element in list
     //if a and b are somewhere in the middle
 }
 
+//TODO
 int List_bubble_sort(List *list, List_compare cmp) {
     check(list, "Can't sort NULL list using compare function.");
     check(cmp, "Can't sort list using NULL compare function.");
 
-    //the problem is how to use LIST_FOREACH here
+    //we cant use LIST_FOREACH because we need to change cur but we cant because there is a hidden _node var that actually effects cur
 
     return 0;
 
@@ -27,6 +27,7 @@ error:
     return NULL; //dont know if i can return NULL or should i exit(1)
 }
 
+//TODO
 List *List_merge_sort(List* list, List_compare cmp) {
     check(list, "Can't sort NULL list using compare function.");
     check(cmp, "Can't sort list using NULL compare function.");
