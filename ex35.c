8916306649d5ee05ccd1043214b7a88edd745f90
NeumnoTele <jure.rot96@gmail.com>
2019-03-24 16:25:54 +0100

    ex35 unions

--- ex35.c
+++ ex35.c
@@ -21,6 +21,15 @@ struct Variant {
 
 typedef struct Variant Variant; //don't know what this is  //don't know what this is
 
+struct Testing { //for testing something
+    int bla;
+    union {
+        int a;
+        int b;
+    } data;
+};
+typedef struct Testing Testing;
+
 void Variant_print(Variant *var) {
     switch (var->type) {
         case TYPE_INT:
@@ -54,6 +63,19 @@ int main(int argc, char *argv[]) {
     Variant_print(&a_int);
     Variant_print(&a_float);
     Variant_print(&a_string);
+
+    Variant test = { .type = TYPE_INT, .data.as_integer = 100 };
+    printf("\ntest int: %d\n", test.data.as_integer);
+    test.data.as_float = 3.14;
+    printf("test int: %d\n", test.data.as_integer);
+    printf("test int: %f\n", test.data.as_float);
+    //you can see that after assigning as_float we muddled the as_int because they share the same memory
+
+    Testing test2 = { .bla = 0, .data.a = 123 };
+    printf("\ntest2 a: %d, b: %d\n", test2.data.a, test2.data.b);
+    test2.data.b = 321;
+    printf("after change test2 a: %d, b: %d\n", test2.data.a, test2.data.b);
+    //I DONT UNDERSTAND HOW WE USE UNION KEY AND VALUE IN RADIXMAP IF ASSIGNING THE FIRST RUINS THE SECOND
 
     return 0;
 }
